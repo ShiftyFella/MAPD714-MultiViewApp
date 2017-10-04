7fa93070814417148dfb9845236666fb510d130e
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-10-04 19:57:01 -0400

    added two more view controllers

---
+++ MultiViewAppTest/BlueViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BlueViewController.swift
+//  MultiViewAppTest
+//
+//  Created by Viktor Bilyk on 2017-10-04.
+//  Copyright © 2017 Shifty Land LLC. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+    override func viewDidLoad() {
+        //
+        
+    }
+}
---
+++ MultiViewAppTest/YellowViewController.swift
@@ -0,0 +1,16 @@
+//
+//  YellowViewController.swift
+//  MultiViewAppTest
+//
+//  Created by Viktor Bilyk on 2017-10-04.
+//  Copyright © 2017 Shifty Land LLC. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+    override func viewDidLoad() {
+        //
+        
+    }
+}
