a0753b2caa9716f985f703ea93ce6aab5a9b9db2
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-10-04 19:43:34 -0400

    Switching View Controller Created basic view deleted, new created

---
+++ MultiViewAppTest/SwitchingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SwitchingViewController.swift
+//  MultiViewAppTest
+//
+//  Created by Viktor Bilyk on 2017-10-04.
+//  Copyright © 2017 Shifty Land LLC. All rights reserved.
+//
+
+import Foundation
