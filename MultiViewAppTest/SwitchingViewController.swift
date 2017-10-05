da57d36efe16adac6a04e58e0b6a96fc1e85e4fc
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-10-04 21:10:07 -0400

    Added switching view controllers logic switching view controller logic, bg for view controllers to identify which is which

--- MultiViewAppTest/SwitchingViewController.swift
+++ MultiViewAppTest/SwitchingViewController.swift
@@ -17,12 +17,74 @@ class SwitchingViewController: UIViewController {
     
     override func viewDidLoad() {
         //
+        super.viewDidLoad()
+        
+        self._blueViewController = storyboard?.instantiateViewController(withIdentifier: "Blue") as! BlueViewController
+        
+        self._blueViewController.view.frame = view.frame
+        switchingViewController(from: nil, to: self._blueViewController)
+        
         
     }
     
+    
     //
     
-    @IBAction func switchViews(sender: UIBarButtonItem) {
+    private func switchingViewController(from fromVC:UIViewController?,
+    to toVC:UIViewController?) {
+        if fromVC != nil {
+            fromVC!.willMove(toParentViewController: nil)
+            fromVC!.view.removeFromSuperview()
+            fromVC!.removeFromParentViewController()
+        }
         
+        if toVC != nil {
+            self.addChildViewController(toVC!)
+            self.view.insertSubview(toVC!.view, at: 0)
+            toVC!.didMove(toParentViewController: self)
+            
+        }
+        
+    }
+    
+    
+    @IBAction func switchViews(sender: UIBarButtonItem) {
+        if self._yellowViewController?.view.superview == nil {
+            //
+            if self._yellowViewController == nil {
+                self._yellowViewController = storyboard?.instantiateViewController(withIdentifier: "Yellow") as! YellowViewController
+            }
+        }
+        else if self._yellowViewController?.view.superview == nil {
+            //
+            if self._yellowViewController == nil {
+                self._blueViewController = storyboard?.instantiateViewController(withIdentifier: "Blue") as! BlueViewController
+            }
+        }
+        
+        if self._blueViewController != nil && self._blueViewController!.view.superview != nil {
+            //
+            self._yellowViewController.view.frame = view.frame
+            switchingViewController(from: self._blueViewController, to: self._yellowViewController)
+            
+        }
+        else {
+            self._blueViewController.view.frame = view.frame
+            switchingViewController(from: self._yellowViewController, to: self._blueViewController)
+        }
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        if self._blueViewController != nil && self._blueViewController!.view.superview == nil {
+            self._blueViewController = nil
+        }
+        
+        if self._yellowViewController != nil && self._yellowViewController!.view.superview == nil {
+            self._yellowViewController = nil
+        }
     }
 }
